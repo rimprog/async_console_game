ed9ce9cd26ec498fba60d1701cd6f20b3be2f01b
riminprog <riminprog@gmail.com>
2023-06-13 14:37:40 +0300

    Remove indexes

--- main.py
+++ main.py
@@ -31,15 +31,13 @@ async def blink(canvas, row, column, symbol='*', offset_tics=0):
             await asyncio.sleep(0)
 
 
-def generate_stars(canvas_size, count):
+def generate_stars(canvas_height, canvas_width, count):
     stars = []
     for _ in range(count):
         symbol = random.choice('+*.:')
-        coordinates = [
-            random.randint(2, canvas_size[0] - 2),
-            random.randint(2, canvas_size[1] - 2)
-        ]
-        stars.append({'symbol': symbol, 'coordinates': coordinates})
+        row_coordinate = random.randint(2, canvas_height - 2)
+        column_coordinate = random.randint(2, canvas_width - 2)
+        stars.append({'symbol': symbol, 'row_coordinate': row_coordinate, 'column_coordinate': column_coordinate})
 
     return stars
 
@@ -74,28 +72,27 @@ async def make_fire(canvas, start_row, start_column, rows_speed=-0.3, columns_sp
         column += columns_speed
 
 
-async def animate_spaceship(canvas, spaceship_frames, coordinates, canvas_size, speed=1):
+async def animate_spaceship(canvas, spaceship_frames, start_row, start_column, canvas_height, canvas_width, speed=1):
+    row, column = start_row, start_column
     min_row = 1
     min_column = 1
-    max_row = canvas_size[0] - 1
-    max_column = canvas_size[1] - 1
-
-    row, column = coordinates
+    max_row = canvas_height - 1
+    max_column = canvas_width - 1
 
     for spaceship_frame in cycle(spaceship_frames):
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
         row += rows_direction * speed
         column += columns_direction * speed
 
-        spaceship_frame_size = get_frame_size(spaceship_frame)
+        frame_rows, frame_columns = get_frame_size(spaceship_frame)
         if row < min_row:
             row = min_row
-        elif row > max_row - spaceship_frame_size[0]:
-            row = max_row - spaceship_frame_size[0]
+        elif row > max_row - frame_rows:
+            row = max_row - frame_rows
         elif column < min_column:
             column = min_column
-        elif column > max_column - spaceship_frame_size[1]:
-            column = max_column - spaceship_frame_size[1]
+        elif column > max_column - frame_columns:
+            column = max_column - frame_columns
 
         for _ in range(2):
             draw_frame(canvas, row, column, spaceship_frame)
@@ -107,24 +104,24 @@ def draw(canvas):
     curses.curs_set(False)
     canvas.border()
     canvas.nodelay(True)
-    canvas_size = curses.window.getmaxyx(canvas)
-    canvas_center_coordinates = (int(canvas_size[0] / 2), int(canvas_size[1] / 2))
+    canvas_height, canvas_width = curses.window.getmaxyx(canvas)
+    canvas_center_row_coordinate, canvas_center_column_coordinate = int(canvas_height / 2), int(canvas_width / 2)
 
-    stars = generate_stars(canvas_size, 1000)
+    stars = generate_stars(canvas_height, canvas_width, 1000)
 
     blinking_stars = []
     for star in stars:
         offset_tics = random.randint(1, 100)
         blinking_star = blink(
             canvas,
-            star['coordinates'][0],
-            star['coordinates'][1],
+            star['row_coordinate'],
+            star['column_coordinate'],
             symbol=star['symbol'],
             offset_tics=offset_tics
         )
         blinking_stars.append(blinking_star)
 
-    shot = make_fire(canvas, canvas_center_coordinates[0] - 1, canvas_center_coordinates[1] + 2)
+    shot = make_fire(canvas, canvas_center_row_coordinate - 1, canvas_center_column_coordinate + 2)
 
     with open('animation_frames/spaceship_frame_1.txt', 'r') as spaceship_frame_1_file:
         spaceship_frame_1 = spaceship_frame_1_file.read()
@@ -133,7 +130,15 @@ def draw(canvas):
         spaceship_frame_2 = spaceship_frame_2_file.read()
 
     spaceship_frames = (spaceship_frame_1, spaceship_frame_2)
-    spaceship = animate_spaceship(canvas, spaceship_frames, canvas_center_coordinates, canvas_size, 10)
+    spaceship = animate_spaceship(
+        canvas,
+        spaceship_frames,
+        canvas_center_row_coordinate,
+        canvas_center_column_coordinate,
+        canvas_height,
+        canvas_width,
+        10
+    )
 
     coroutines = [*blinking_stars, shot, spaceship]
 
